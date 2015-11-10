411bbfa634a079d520f085f8277d96af82a5248e
Cezar Sa Espinola <cezarsa@gmail.com>
2015-11-10 16:04:14 -0200

    add dockerfile and travis config

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+# Copyright 2015 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+FROM alpine:3.2
+ADD planb /bin/planb
+ENTRYPOINT ["/bin/planb"]
---
+++ travis/docker_image.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKER_TAG=$(([ "${TRAVIS_BRANCH}" = "master" ] && echo latest) || ([ "${TRAVIS_BRANCH}" = "v1" ] && echo v1))
+
+if [ -n "${DOCKER_TAG}" ] && [ "${TRAVIS_GO_VERSION}" = "${TRAVIS_GO_VERSION}" ]; then
+	cat > ~/.dockercfg <<EOF
+{
+  "https://index.docker.io/v1/": {
+    "auth": "${HUB_AUTH}",
+    "email": "${HUB_EMAIL}"
+  }
+}
+EOF
+	docker build -t tsuru/planb:${DOCKER_TAG} .
+	docker push tsuru/planb:${DOCKER_TAG}
+else
+	echo "No image to build"
+fi
