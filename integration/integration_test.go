ca33491edbda4f26718bb3398b187e1efffbf919
Cezar Sa Espinola <cezarsa@gmail.com>
2016-11-11 17:52:07 -0200

    integration: add integration benchmark

---
+++ integration/integration_test.go
@@ -0,0 +1,108 @@
+package integration
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/tsuru/planb/backend"
+	"github.com/tsuru/planb/reverseproxy"
+	"github.com/tsuru/planb/router"
+	"gopkg.in/redis.v3"
+)
+
+const redisDB = 5
+
+func clearKeys(r *redis.Client) error {
+	val := r.Keys("frontend:*").Val()
+	val = append(val, r.Keys("dead:*").Val()...)
+	if len(val) > 0 {
+		return r.Del(val...).Err()
+	}
+	return nil
+}
+
+func redisConn() (*redis.Client, error) {
+	return redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", DB: redisDB}), nil
+}
+
+func initTest(t testing.TB) *redis.Client {
+	redis, err := redisConn()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = clearKeys(redis)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return redis
+}
+
+func BenchmarkFullStackNativeRedisNoCache(b *testing.B) {
+	b.StopTimer()
+	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+	}))
+	r := initTest(b)
+	backends := make([]string, 100)
+	defer srv.Close()
+	for i := range backends {
+		backends[i] = srv.URL
+	}
+	backends = append([]string{"benchfrontend"}, backends...)
+	err := r.RPush("frontend:myfrontend.com", backends...).Err()
+	if err != nil {
+		b.Fatal(err)
+	}
+	rp := &reverseproxy.NativeReverseProxy{}
+	opts := backend.RedisOptions{
+		DB: redisDB,
+	}
+	be, err := backend.NewRedisBackend(opts, opts)
+	if err != nil {
+		b.Fatal(err)
+	}
+	router := router.Router{
+		Backend: be,
+	}
+	err = router.Init()
+	if err != nil {
+		b.Fatal(err)
+	}
+	err = rp.Initialize(reverseproxy.ReverseProxyConfig{
+		Router: &router,
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		b.Fatal(err)
+	}
+	url := fmt.Sprintf("http://%s/", listener.Addr().String())
+	go rp.Listen(listener)
+	defer rp.Stop()
+	cli := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 1000,
+		},
+	}
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			request, _ := http.NewRequest("GET", url, nil)
+			request.Host = "myfrontend.com"
+			rsp, err := cli.Do(request)
+			if rsp == nil || rsp.StatusCode != http.StatusOK {
+				b.Fatalf("invalid response %#v: %s", rsp, err)
+			}
+			io.Copy(ioutil.Discard, rsp.Body)
+			rsp.Body.Close()
+		}
+	})
+	b.StopTimer()
+}
